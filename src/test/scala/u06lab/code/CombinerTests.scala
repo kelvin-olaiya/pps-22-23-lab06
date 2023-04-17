01827c11b7e2d713ddf8a4a0fce2024620e6b54f
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2023-04-17 13:37:59 +0200

    chore: complete exercise 1

---
+++ src/test/scala/u06lab/code/CombinerTests.scala
@@ -0,0 +1,15 @@
+package u06lab.code
+
+import org.junit.Test
+import org.junit.Assert.*
+
+class CombinerTests:
+  @Test
+  def testFunctions(): Unit = 
+    val f: Functions = FunctionsImpl
+    assertEquals(60.1, f.sum(List(10.0, 20.0, 30.1)), 0.001) // 60.1
+    assertEquals(0.0, f.sum(List()), 0.1)
+    assertEquals("abc", f.concat(Seq("a", "b", "c")))
+    assertEquals("", f.concat(Seq()))
+    assertEquals(3, f.max(List(-10, 3, -5, 0)))
+    assertEquals(Integer.MIN_VALUE, f.max(List()))

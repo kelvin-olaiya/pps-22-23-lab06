42696c71087ea399caceadfe8b690e7abcd3329e
Kelvin Olaiya <kelvinoluwada.olaiya@studio.unibo.it>
2023-04-23 12:35:44 +0200

    chore(ConnectThree): reuse TwoPlayerBoardGame

--- src/main/scala/u06lab/code/ConnectThree.scala
+++ src/main/scala/u06lab/code/ConnectThree.scala
@@ -3,90 +3,41 @@ package u06lab.code
 import java.util.OptionalInt
 
 // Optional!
-object ConnectThree extends App:
-  val bound = 3
-  enum Player:
-    case X, O
-    def other: Player = this match
-      case X => O
-      case _ => X
-
-  case class Disk(x: Int, y: Int, player: Player)
-  /**
-   * Board:
-   * y
-   *
-   * 3
-   * 2
-   * 1
-   * 0
-   *   0 1 2 3 <-- x
-   */
-  type Board = Seq[Disk]
-  type Game = Seq[Board]
-
-  import Player.*
-
-  def find(board: Board, x: Int, y: Int): Option[Player] = board.find(disk => disk.x == x && disk.y == y).map(_.player)
-
+object ConnectThree extends TwoPlayerBoardGame with App:
+  import GameCore.{Board, Placement, Player}
+  import GameCore.Player.*
+  override val BOARD_SIZE = 4
+  
   def firstAvailableRow(board: Board, x: Int): Option[Int] =
-    board.collect { case Disk(`x`, y, _) => y }.maxOption.map(_ + 1).orElse(Some(0)).filter(_ <= bound)
-
-  def placeAnyDisk(board: Board, player: Player): Seq[Board] =
+    board.collect { case Placement(`x`, y, _) => y }.maxOption.map(_ + 1).orElse(Some(0)).filter(_ <= BOARD_SIZE)
+    
+  override def anyPlacement(board: Board, player: Player): Seq[Board] =
     for
-      x <- 0 to bound
+      x <- 0 until BOARD_SIZE
       y = firstAvailableRow(board, x)
       if y.isDefined
-    yield Disk(x, y.get, player) +: board
-
-  def computeAnyGame(player: Player, moves: Int): LazyList[Game] = moves match
-    case 0 => LazyList[Game](List(List()))
-    case _ =>
-      (for
-        game <- computeAnyGame(player.other, moves - 1)
-        previousBoard = game.head
-        board <- placeAnyDisk(previousBoard, player)
-      yield if previousBoard.isWinning then game else board +: game).distinct
-
-  extension (board: Board)
-    def isWinning: Boolean =
-      board.exists(disk =>
-        List((0, 1), (1, 0), (1, 1), (-1, 1)).map(offset => List(offset, (-offset._1, -offset._2)))
-          .map(direction => direction.map(position => find(board, position._1 + disk.x, position._2 + disk.y)))
-          .exists(direction => direction.forall(player => player.isDefined && player.get == disk.player))
-      )
-
-  def printBoards(game: Seq[Board]): Unit =
-    for
-      y <- bound to 0 by -1
-      board <- game.reverse
-      x <- 0 to bound
-    do
-      print(find(board, x, y).map(_.toString).getOrElse("."))
-      if x == bound then
-        print(" ")
-        if board == game.head then println()
+    yield Placement(x, y.get, player) +: board
 
   // Exercise 1: implement find such that..
   println("EX 1: ")
-  println(find(List(Disk(0, 0, X)), 0, 0)) // Some(X)
-  println(find(List(Disk(0, 0, X), Disk(0, 1, O), Disk(0, 2, X)), 0, 1)) // Some(O)
-  println(find(List(Disk(0, 0, X), Disk(0, 1, O), Disk(0, 2, X)), 1, 1)) // None
+  println(find(List(Placement(0, 0, X)), 0, 0)) // Some(X)
+  println(find(List(Placement(0, 0, X), Placement(0, 1, O), Placement(0, 2, X)), 0, 1)) // Some(O)
+  println(find(List(Placement(0, 0, X), Placement(0, 1, O), Placement(0, 2, X)), 1, 1)) // None
 
   // Exercise 2: implement firstAvailableRow such that..
   println("EX 2: ")
   println(firstAvailableRow(List(), 0)) // Some(0)
-  println(firstAvailableRow(List(Disk(0, 0, X)), 0)) // Some(1)
-  println(firstAvailableRow(List(Disk(0, 0, X), Disk(0, 1, X)), 0)) // Some(2)
-  println(firstAvailableRow(List(Disk(0, 0, X), Disk(0, 1, X), Disk(0, 2, X)), 0)) // Some(3)
-  println(firstAvailableRow(List(Disk(0, 0, X), Disk(0, 1, X), Disk(0, 2, X), Disk(0, 3, X)), 0)) // None
+  println(firstAvailableRow(List(Placement(0, 0, X)), 0)) // Some(1)
+  println(firstAvailableRow(List(Placement(0, 0, X), Placement(0, 1, X)), 0)) // Some(2)
+  println(firstAvailableRow(List(Placement(0, 0, X), Placement(0, 1, X), Placement(0, 2, X)), 0)) // Some(3)
+  println(firstAvailableRow(List(Placement(0, 0, X), Placement(0, 1, X), Placement(0, 2, X), Placement(0, 3, X)), 0)) // None
   // Exercise 2: implement placeAnyDisk such that..
-  printBoards(placeAnyDisk(List(), X))
+  printBoards(anyPlacement(List(), X))
   // .... .... .... ....
   // .... .... .... ....
   // .... .... .... ....
   // ...X ..X. .X.. X...
-  printBoards(placeAnyDisk(List(Disk(0, 0, O)), X))
+  printBoards(anyPlacement(List(Placement(0, 0, O)), X))
   // .... .... .... ....
   // .... .... .... ....
   // ...X .... .... ....
